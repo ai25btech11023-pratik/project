a7782cd052df5418b87cc88165965f7d7c0fc818
Pratik R <pratikraj5228@gmail.com>
2025-09-18 17:35:25 +0530

    Initial Commit

---
+++ ITP/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ITP
+//
+//  Created by Pratik Raj on 18/09/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
